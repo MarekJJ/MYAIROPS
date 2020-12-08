0b8a26ba488e35f8037a0ec63c50683763fb9214
Marek <marekjedrysiak@gmail.com>
2020-12-08 17:33:21 +0100

    first

---
+++ MYAIROPS.Web/Models/ModelClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MYAIROPS.Web.Models
+{
+    public class ModelClass
+    {
+        public string TextBoxOne { get; set; }
+
+        public string TextBoxTwo { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
