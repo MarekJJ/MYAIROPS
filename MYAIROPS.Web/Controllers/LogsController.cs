3eae8e3f3bb980e3a4033dc4f2b07b6f50ca5c25
Marek <marekjedrysiak@gmail.com>
2020-12-08 22:36:48 +0100

    added helpers

---
+++ MYAIROPS.Web/Controllers/LogsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MYAIROPS.Web.Interfaces;
+using MYAIROPS.Web.Models;
+
+namespace MYAIROPS.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LogsController : ControllerBase
+    {
+        private readonly ILogsService _logsService;
+        public LogsController(ILogsService logsService)
+        {
+            ILogsService _logsService = logsService;
+        }
+
+        [HttpPost("{log}")]
+        public ActionResult AddLog(Log log)
+        {
+            throw new System.NotImplementedException();
+        }
+
+    }
+}
--- MYAIROPS.Web/Startup.cs
+++ MYAIROPS.Web/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using MYAIROPS.Web.Dependency;
 
 namespace MYAIROPS.Web
 {
@@ -25,6 +26,7 @@ namespace MYAIROPS.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            Dependency.Injector(services);
             services.AddControllers();
         }
 
